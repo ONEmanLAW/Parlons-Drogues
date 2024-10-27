dff7ea94f5d1544602ba1589295f21772afd00d0
Hugo Culot <hugoculot.dev@gmail.com>
2024-10-27 14:45:03 +0100

    Added GSAP & D3.js to project and added 1st component Template of Card turn view.

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card" @click="toggleFlip" ref="cardRef">
+    <div :class="['card-inner', { flipped }]">
+      <div class="card-front">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+      </div>
+      <div class="card-back">
+        <h3>Back Side</h3>
+        <p>Some additional information here.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { gsap } from 'gsap';
+
+const props = defineProps({
+  title: String,
+  description: String,
+});
+
+const flipped = ref(false);
+
+
+const cardRef = ref(null);
+
+const toggleFlip = () => {
+  flipped.value = !flipped.value;
+
+  
+  gsap.to(cardRef.value.querySelector('.card-inner'), {
+    rotateY: flipped.value ? 180 : 0,
+    duration: 0.6,
+    ease: "power1.inOut",
+  });
+};
+</script>
+
+<style src="../styles/Card.css"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
     <h1>Bienvenue sur Parlons Drogues</h1>
-    <p>Ceci est une plateforme dédiée à la visualisation des données sur les drogues.</p>
+    <p>Voici des informations importantes.</p>
+    
+    <!-- Section avec les cartes -->
+    <div class="card-container">
+      <Card title="Carte 1" description="Description de la carte 1" />
+      <Card title="Carte 2" description="Description de la carte 2" />
+      <Card title="Carte 3" description="Description de la carte 3" />
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import Card from '../components/Card.vue';
 </script>
 
-<style src="../styles/Home.css" scoped></style>
+
+<style src="../styles/Home.css"></style>
