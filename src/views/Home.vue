a7ebc8c223aa37fa0b4e33ad382d40788bfbf819
Hugo Culot <hugoculot.dev@gmail.com>
2024-11-03 22:38:29 +0100

    Added quiz feature with final results pie chart visualization - added chart.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Bienvenue sur Parlons Drogues</h1>
     <p>Voici des informations importantes.</p>
-    
+
     <div class="card-container">
       <Card
         v-for="(card, index) in cards"
@@ -10,11 +10,16 @@
         :title="card.title"
         :description="card.description"
         :backContent="card.backContent"
+        :backgroundImage="card.backgroundImage"
       />
     </div>
 
     <div class="interactive-section">
       <InteractiveCharacter />
+    </div>
+
+    <div class="quiz-section">
+      <Quiz />
     </div>
   </div>
 </template>
@@ -23,12 +28,11 @@
 import { ref, onMounted } from 'vue';
 import Card from '../components/Card.vue';
 import InteractiveCharacter from '../components/InteractiveCharacter.vue';
-
+import Quiz from '../components/Quiz.vue';
 
 import cardsData from '../data/cardsData.json';
 
 const cards = ref([]);
-
 
 onMounted(() => {
   cards.value = cardsData.cards;
