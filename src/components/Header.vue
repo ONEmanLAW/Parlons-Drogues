9eeb715132d6173f7823ea4c7534ce21152da658
Hugo Culot <hugoculot.dev@gmail.com>
2024-11-07 14:45:33 +0100

    Fixed Header with new li faut Intro and Home.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,8 @@
     <h1>Parlons Drogues</h1>
     <nav>
       <ul>
-        <li><router-link to="/">Accueil</router-link></li>
+        <li><router-link to="/">Intro</router-link></li>
+        <li><router-link to="/home">Home</router-link></li>
         <li><router-link to="/chapter1">Chapitre 1</router-link></li>
         <li><router-link to="/chapter2">Chapitre 2</router-link></li>
         <li><router-link to="/chapter3">Chapitre 3</router-link></li>
