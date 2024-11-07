433b3ce4c4509adf302576c4d3a6af103358d929
Hugo Culot <hugoculot.dev@gmail.com>
2024-11-07 16:26:20 +0100

    Added router function for when website is finish

--- src/router/index.js
+++ src/router/index.js
@@ -21,3 +21,42 @@ const router = createRouter({
 });
 
 export default router;
+
+
+
+// Quand le site sera Finit
+
+// import { createRouter, createWebHistory } from 'vue-router';
+// import Home from '../views/Home.vue';
+// import Chapter1 from '../views/Chapter1.vue';
+// import Chapter2 from '../views/Chapter2.vue';
+// import Chapter3 from '../views/Chapter3.vue';
+// import Advice from '../views/Advice.vue';
+// import Intro from '../views/IntroPage.vue';
+
+// const routes = [
+//   { path: '/', component: Intro },
+//   { path: '/home', component: Home },
+//   { path: '/chapter1', component: Chapter1 },
+//   { path: '/chapter2', component: Chapter2 },
+//   { path: '/chapter3', component: Chapter3 },
+//   { path: '/advice', component: Advice },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+//   scrollBehavior() {
+//     return { top: 0 }; // Retourne toujours en haut de la page
+//   }
+// });
+
+// // Redirection après chaque navigation
+// router.afterEach(() => {
+//   // Remplace l'URL visible par l'URL principale
+//   window.history.replaceState(null, '', '/');
+// });
+
+// export default router;
+
+
