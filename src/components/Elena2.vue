e2281b97c8f5eaecdc16a6f296ea095661a19e16
Hugo Culot <hugoculot.dev@gmail.com>
2024-12-02 19:39:24 +0100

    Added new css to info2

--- src/components/Elena2.vue
+++ src/components/Elena2.vue
@@ -3,17 +3,16 @@
     <div :class="`text-section`">
       <div
         :class="`info-text`"
-        :style="{ color: currentChapterData.infoTextColor, backgroundColor: currentChapterData.infoBackgroundColor }"
+        :style="{ backgroundColor: infoBackgroundColor, color: infoTextColor }"
       >
         Informations
       </div>
-      <h1>
-        {{ currentChapterData.title.split(',')[0] }},<br />
-        <span :style="{ color: currentChapterData.highlightOrange }">{{ currentChapterData.title.split(',')[1] }}</span>
+      <h1 :style="{ color: titleColor }">
+        {{ currentChapterData.title.split(',')[0] }}
+        <br />
+        <span :style="{ color: currentChapterData.highlightBlue }">{{ currentChapterData.title.split(',')[1] }}</span>
       </h1>
-      <p>
-        {{ currentChapterData.paragraph }}
-      </p>
+      <p v-html="highlightText(currentChapterData.paragraph)"></p>
     </div>
     <div :class="`image-section`">
       <img class="main-image" :src="currentChapterData.image" alt="Illustration" />
@@ -39,6 +38,75 @@ export default {
     const currentChapterData = computed(() =>
       chapters.chapters.find((chapter) => chapter.id === props.currentChapter)
     );
+
+    const titleColor = computed(() => {
+      if (props.currentChapter === 2) {
+        return "#167540";
+      } else if (props.currentChapter === 3) {
+        return "#AC0266"; 
+      } else if (props.currentChapter === 1) {
+        return "#3135B7"; 
+      } else {
+        return "#2e2f85";
+      }
+    });
+
+  
+    const infoTextColor = computed(() => {
+      if (props.currentChapter === 1) {
+        return "#3135B7"; 
+      } else if (props.currentChapter === 2) {
+        return "#167540"; 
+      } else if (props.currentChapter === 3) {
+        return "#AC0266";
+      } else {
+        return "#333333";
+      }
+    });
+
+    const infoBackgroundColor = computed(() => {
+      if (props.currentChapter === 1) {
+        return "#A4E1FF"; 
+      } else if (props.currentChapter === 2) {
+        return "#BCFFC8"; 
+      } else if (props.currentChapter === 3) {
+        return "#FFC9EA"; 
+      } else {
+        return "#FFFFFF"; 
+      }
+    });
+
+    const highlightText = (text) => {
+      let wordsToHighlight;
+
+      if (props.currentChapter === 1) {
+        wordsToHighlight = ['joint', '20 clopes', 'risque'];
+      } else if (props.currentChapter === 2) {
+        wordsToHighlight = ['Se débarrasser', 'sevrage', 'rechutent'];
+      } else if (props.currentChapter === 3) {
+        wordsToHighlight = ['cerveau', 'concentrer', 'souvenir', 'réfléchir'];
+      }
+
+      wordsToHighlight.forEach(word => {
+        const regEx = new RegExp(`(${word})`, 'gi');
+        const color = getColorForWord(word); 
+        text = text.replace(regEx, `<span style="color: ${color}; font-weight: bold;">$1</span>`);
+      });
+
+      return text;
+    };
+
+    const getColorForWord = (word) => {
+      if (props.currentChapter === 1) {
+        return "#3135B7"; 
+      } else if (props.currentChapter === 2) {
+        return "#167540"; 
+      } else if (props.currentChapter === 3) {
+        return "#AC0266"; 
+      } else {
+        return "#333333";
+      }
+    };
 
     onMounted(() => {
       gsap.registerPlugin(ScrollTrigger);
@@ -93,12 +161,15 @@ export default {
     });
 
     return {
-      currentChapterData
+      currentChapterData,
+      titleColor,
+      infoTextColor,
+      infoBackgroundColor,
+      highlightText
     };
   }
 };
 </script>
-
 
 <style scoped>
 .container {
@@ -124,30 +195,19 @@ export default {
 
 .info-text {
   display: inline-block;
-  font-size: clamp(0.8rem, 1.2vw, 1rem);
+  font-size: 14px; 
   font-weight: bold;
-  color: #ffffff;
-  background-color: #5eafef;
-  padding: 5px 15px;
-  border-radius: 20px;
-  margin-bottom: 15px;
+  padding: 5px 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
   text-transform: uppercase;
 }
 
 .text-section h1 {
   font-size: clamp(1.8rem, 3.5vw, 2.4rem);
   line-height: 1.4;
-  color: #2e2f85;
   margin: 0;
   font-weight: bold;
-}
-
-.text-section h1 .highlight-orange {
-  color: #ff6d00;
-}
-
-.text-section h1 .highlight-blue {
-  color: #2e2f85;
 }
 
 .text-section p {
@@ -158,7 +218,6 @@ export default {
 }
 
 .text-section p .highlight {
-  color: #2e2f85;
   font-weight: bold;
 }
 
